7c75b9a56a2ef7bc088aee174f572ce4f1606ce2
parasaiteve <101370780+parasaiteve@users.noreply.github.com>
2023-06-11 16:24:13 -0300

    update

---
+++ do_while.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+
+        // usar do-while para executar o código
+        do
+        {
+            Console.Write("Digite um número de 1 a 10: ");
+            num = int.Parse(Console.ReadLine());
+            // while para ser usada em condição
+        } while (num < 1 || num > 10);
+
+        Console.WriteLine("Voce digitou o numero " + num + ".");
+
+        // fechamento do programa
+        Console.ReadLine();
+    }
+}
