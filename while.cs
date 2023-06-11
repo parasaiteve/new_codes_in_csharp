d88e83293b55fb39f429fa535a50e8d10b63e911
parasaiteve <101370780+parasaiteve@users.noreply.github.com>
2023-06-11 16:19:42 -0300

    update

---
+++ while.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num, i = 1;
+
+        // interface de usuário
+        Console.Write("Digite um número inteiro positivo: ");
+        num = int.Parse(Console.ReadLine());
+
+        // usar while enquanto o número for maior ou igual a i
+        while (i <= num)
+        {
+            // mostrar a contagem de 1 até i
+            Console.Write(i + " ");
+            i++;
+        }
+
+        // fechamento do programa
+        Console.ReadLine();
+    }
+}
